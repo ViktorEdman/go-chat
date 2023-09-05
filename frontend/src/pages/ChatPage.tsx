4458c75b502f627e563faf6db6f8a4e394fd7d22
Viktor Edman <viktor@edman.se>
2023-09-05 16:06:12 +0200

    presence detection complete, design update

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -44,7 +44,7 @@ export default function ChatPage() {
         <div className="col-span-3 row-span-3">
           <div className="h-[90%] flex flex-col overflow-y-auto">
             {messages.map((message: string) => (
-              <span className="w-full px-2">{message}</span>
+              <span className="w-full px-2" key={message}>{message}</span>
             ))}
           </div>
           <form
